50caab7d2dfa00d666ae40140967bbe967f9658f
PuneethKumar01 <puneethkumar1405@gmail.com>
2024-11-03 19:08:09 +0530

    update

--- app/page.js
+++ app/page.js
@@ -8,25 +8,41 @@ const page = () => {
 
   let res;
   const sun = async (loc) => {
-    res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=metric&appid=aedb9cd52d9f17211801b18c296851e8`)
-    console.log(res);
-    let obj = await res.json()
-    console.log(obj)
-    let address = await fetch(` https://geocode.maps.co/reverse?lat=${obj.coord.lat}&lon=${obj.coord.lon}&api_key=672460df18a97492686832tpj473b0d`)
-    let finalAddress = await address.json();
-    console.log(finalAddress);
 
     setWeatherResult(<>
-      <div className='bg-neutral-200 px-10 py-5 rounded-lg'>
-        <img src={`https://flagsapi.com/${obj.sys.country}/flat/64.png`} alt={obj.sys.country} width='100px' height='100px' />
-        <p className='text-xl'>Place :  {finalAddress.display_name}</p>
-        <p className='text-xl font-semibold'>Temperature :  {obj.main.temp}</p>
-        <p className='text-2xl font-semibold'>Weather :  {obj.weather[0].main}</p>
-        <p className='text-xl'>Humidity :  {obj.main.humidity}</p>
-        <p className='text-xl'>Wind : {(obj.wind.speed * 3.6).toFixed(2)} km/hr</p>
+      <div className='bg-neutral-200 px-10 py-16 rounded-lg '>
+        <div className='h-32 w-32 rounded-full bg-transparent m-auto border-neutral-500 border-t-black border-8 animate-spin'></div>
       </div>
     </>)
-    console.log(obj.weather[0].main);
+
+    res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=metric&appid=aedb9cd52d9f17211801b18c296851e8`)
+      .then( async (res) => {
+        console.log(res);
+        let obj = await res.json()
+        console.log(obj)
+        let address = await fetch(` https://geocode.maps.co/reverse?lat=${obj.coord.lat}&lon=${obj.coord.lon}&api_key=672460df18a97492686832tpj473b0d`)
+        let finalAddress = await address.json();
+        console.log(finalAddress);
+
+        setWeatherResult(<>
+          <div className='bg-neutral-200 px-10 py-5 rounded-lg'>
+            <img src={`https://flagsapi.com/${obj.sys.country}/flat/64.png`} alt={obj.sys.country} width='100px' height='100px' />
+            <p className='text-xl'>Place :  {finalAddress.display_name}</p>
+            <p className='text-xl font-semibold'>Temperature :  {obj.main.temp}</p>
+            <p className='text-2xl font-semibold'>Weather :  {obj.weather[0].main}</p>
+            <p className='text-xl'>Humidity :  {obj.main.humidity}</p>
+            <p className='text-xl'>Wind : {(obj.wind.speed * 3.6).toFixed(2)} km/hr</p>
+          </div>
+        </>)
+        console.log(obj.weather[0].main);
+      }).catch((err)=>{
+        setWeatherResult(<>
+              <div className='bg-neutral-200 px-10 py-16 rounded-lg '>
+        <p className='font-bold uppercase text-center text-2xl'>invalid address</p>
+      </div>
+        </>)
+      })
+
   }
 
 
